a6ed18db7336d4418676b70e4356c46b7fbbc18d
ariobarzandarvish18 <aryobarzandarvish@gmail.com>
2023-11-11 19:05:38 +0330

    UPDATED

--- model/da/like_da.py
+++ model/da/like_da.py
@@ -1,18 +1,37 @@
 from tools.database import Database
+from model.entity import  like
 
-
-class LikeDa(Database):
+class likeDa(Database):
     def save(self, like):
-        pass
+        self.transaction("INSERT INTO like_tbl (profile,post) values (%s,%s)",
+                          [like.profile,like.post])
+        return like
 
     def edit(self, like):
-        pass
+        self.transaction("UPDATE like_tbl set profile=%s,post=%s  where  code=%s"
+                         ,[like.profile,like.post])
+        return like
+
 
     def remove(self, code):
-        pass
+        self.transaction("DELETE FROM like_tbl where code=%s",
+                         [code])
+        return code
 
     def find_all(self):
-        pass
+        return self.report("SELECT * FROM like_tbl")
+
 
     def find_by_code(self, code):
-        pass
+        return self.report("SELECT * FROM like_tbl where code=%s",[code])
+
+    def find_by_profile(self,profile):
+        return self.report("SELECT * FROM like_tbl where profile=%s",[like.profile])
+
+
+    def find_by_post(self,post):
+        return self.report("SELECT * FROM like_tbl where post=%s",[like.post])
+
+
+
+
