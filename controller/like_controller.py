d7057317e4934bc405ab3c50577e01d064313021
Student <sogolbasere@gmail.com>
2023-11-13 17:51:35 +0330

    updated

--- controller/like_controller.py
+++ controller/like_controller.py
@@ -1,81 +1,79 @@
-from model.da.like_da import likeDa
-from model.entity.like import Post
+from model.da.like_da import LikeDa
+from model.entity.like import Like
+
 
 class LikeController:
 
     @classmethod
     def save(cls, post, profile):
         try:
-          person = post(None, profile, post)
-          da = likeDa()
-          return True, da.save(post)
+            person = post(None, profile, post)
+            da = LikeDa()
+            return True, da.save(post)
         except Exception as e:
-           return False, str(e)
+            return False, str(e)
 
     @classmethod
     def edit(cls, code, profile, post):
-      try:
-         person = likeDa(code, profile, post)
-         da = likeDa()
-         return True, da.edit(person)
-      except Exception as e:
-        return False, str(e)
-
+        try:
+            like = Like(code, profile, post)
+            da = LikeDa()
+            return True, da.edit(profile)
+        except Exception as e:
+            return False, str(e)
 
     @classmethod
     def remove(cls, code):
-      try:
-        da = likeDa()
-        return True, da.remove(code)
-      except Exception as e:
-         return False, str(e)
-
+        try:
+            da = LikeDa()
+            return True, da.remove(code)
+        except Exception as e:
+            return False, str(e)
 
     @classmethod
     def find_all(cls):
-      try:
-        da = likeDa()
-        return True, da.find_all()
-      except Exception as e:
-        return False, str(e)
-
+        try:
+            da = LikeDa()
+            return True, da.find_all()
+        except Exception as e:
+            return False, str(e)
 
     @classmethod
     def find_by_code(cls, code):
-      try:
-        da = likeDa()
-        return True, da.find_by_code(code)
-      except Exception as e:
-        return False, str(e)
-
+        try:
+            da = LikeDa()
+            return True, da.find_by_code(code)
+        except Exception as e:
+            return False, str(e)
 
     @classmethod
     def find_by_post(cls, post):
-       try:
-          da =likeDa()
-          return True, da.find_by_post(post)
-       except Exception as e:
-          return False, str(e)
+        try:
+            da = LikeDa()
+            return True, da.find_by_post(post)
+        except Exception as e:
+            return False, str(e)
 
     @classmethod
-    def find_by_profile(cls,profile):
-      try:
-        da=likeDa()
-        return True,da.find_by_profile_id(profile)
-      except Exception as e:
-        return False, str(e)
+    def find_by_profile(cls, profile):
+        try:
+            da = LikeDa()
+            return True, da.find_by_profile_id(profile)
+        except Exception as e:
+            return False, str(e)
 
     @classmethod
-    def find_by_username(cls,username):
-       try:
-         da=likeDa()
-         return True,da.find_by_username(username)
-       except Exception as e:
-         return False, str(e)
+    def find_by_username(cls, username):
+        try:
+            da = LikeDa()
+            return True, da.find_by_username(username)
+        except Exception as e:
+            return False, str(e)
 
-def find_by_image(cls,image):
+
+def find_by_image(cls, image):
     try:
-        da=likeDa
-        return True,da.find_by_image(image)
+        da = LikeDa()
+        return True, da.find_by_image(image)
     except Exception as e:
-        return False,str(e)
+        return False, str(e)
--- model/da/like_da.py
+++ model/da/like_da.py
@@ -1,45 +1,35 @@
-from tools.database import Database
-from model.entity import  like
-
-class likeDa(Database):
-    def save(self, like):
-        self.transaction("INSERT INTO like_tbl (profile_id,post_id) values (%s,%s)",
-                          [like.profile_id,like.post_id])
-        return like
-
-    def edit(self, like):
-        self.transaction("UPDATE like_tbl set profile_id=%s,post_id=%s  where  code=%s"
-                         ,[like.profile,like.post_id])
-        return like
+from model.da.database import DatabaseManager
+from model.entity.like import Like
 
 
-    def remove(self, code):
-        self.transaction("DELETE FROM like_tbl where code=%s",
-                         [code])
-        return code
-
-    def find_all(self):
-        return self.report("SELECT * FROM like_tbl")
-
-
-    def find_by_code(self, code):
-        return self.report("SELECT * FROM like_tbl where code=%s",[code])
-
-    def find_by_profile_id(self,profile):
-        return self.report("SELECT * FROM like_tbl where profile_id=%s",[like.profile_id])
+class LikeDa(DatabaseManager):
+    def find_by_profile_id(self,profile_id):
+        self.make_engine()
+        result=self.session.query(Like).filter(Like.profile.id == profile_id)
+        self.session.close()
+        return result
 
 
     def find_by_post_id(self,post_id):
-        return self.report("SELECT * FROM like_tbl where post_id=%s",[like.post_id])
+        self.make_engine()
+        result = self.session.query(Like).filter(Like.post.id == post_id)
+        self.session.close()
+        return result
 
 
 
-    def find_by_username(self,username):
-        return self.report("SELECT * FROM like_tbl where username_id=%s",[like.username])
+    def find_by_username(self, username):
+        self.make_engine()
+        result = self.session.query(Like).filter(Like.profile.username == username)
+        self.session.close()
+        return result
     
     
-    def find_by_image(self):
-        return self.report("SELECT * FROM like_tbl where image=%s",[like.image])
+    def find_by_image(self, image):
+        self.make_engine()
+        result = self.session.query(Like).filter(Like.post.image == image)
+        self.session.close()
+        return result
 
 
 
