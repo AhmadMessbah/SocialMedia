7c82e2ac5b57a53c3c0230ac6dfbf97a1e5f583b
mobina <roudgarmobina@gmail.com>
2023-11-13 14:37:21 +0330

    update

--- controller/like_controller.py
+++ controller/like_controller.py
@@ -1,78 +1,77 @@
 from model.da.like_da import likeDa
 from model.entity.like import Post
 
+class LikeController:
 
+    @classmethod
+    def save(cls, post, profile):
+        try:
+          person = post(None, profile, post)
+          da = likeDa()
+          return True, da.save(post)
+        except Exception as e:
+           return False, str(e)
 
-@classmethod
-def save(cls, post, profile):
-    try:
-        person = post(None, profile, post)
-        da = likeDa()
-        return True, da.save(post)
-    except Exception as e:
+    @classmethod
+    def edit(cls, code, profile, post):
+      try:
+         person = likeDa(code, profile, post)
+         da = likeDa()
+         return True, da.edit(person)
+      except Exception as e:
         return False, str(e)
 
 
-@classmethod
-def edit(cls, code, profile, post):
-    try:
-        person = likeDa(code, profile, post)
-        da = likeDa()
-        return True, da.edit(person)
-    except Exception as e:
-        return False, str(e)
-
-
-@classmethod
-def remove(cls, code):
-    try:
+    @classmethod
+    def remove(cls, code):
+      try:
         da = likeDa()
         return True, da.remove(code)
-    except Exception as e:
-        return False, str(e)
+      except Exception as e:
+         return False, str(e)
 
 
-@classmethod
-def find_all(cls):
-    try:
+    @classmethod
+    def find_all(cls):
+      try:
         da = likeDa()
         return True, da.find_all()
-    except Exception as e:
+      except Exception as e:
         return False, str(e)
 
 
-@classmethod
-def find_by_code(cls, code):
-    try:
+    @classmethod
+    def find_by_code(cls, code):
+      try:
         da = likeDa()
         return True, da.find_by_code(code)
-    except Exception as e:
+      except Exception as e:
         return False, str(e)
 
 
-@classmethod
-def find_by_post(cls, post):
-    try:
-        da =likeDa()
-        return True, da.find_by_post(post)
-    except Exception as e:
-        return False, str(e)
+    @classmethod
+    def find_by_post(cls, post):
+       try:
+          da =likeDa()
+          return True, da.find_by_post(post)
+       except Exception as e:
+          return False, str(e)
 
-
-def find_by_profile(cls,profile):
-    try:
+    @classmethod
+    def find_by_profile(cls,profile):
+      try:
         da=likeDa()
         return True,da.find_by_profile_id(profile)
-    except Exception as e:
+      except Exception as e:
         return False, str(e)
 
-
-def find_by_username(cls,username):
-    try:
-        da=likeDa()
-        return True,da.find_by_username(username)
-    except Exception as e:
-        return False, str(e)
+    @classmethod
+    def find_by_username(cls,username):
+       try:
+         da=likeDa()
+         return True,da.find_by_username(username)
+       except Exception as e:
+         return False, str(e)
 
 def find_by_image(cls,image):
     try:
