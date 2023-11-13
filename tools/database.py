cd979a55248bfe28f4dc07ec17907a1dd533ed03
Behnam Masoumi <150528091+behnamlive@users.noreply.github.com>
2023-11-13 13:36:49 +0330

    profile.py

--- tools/database.py
+++ tools/database.py
@@ -2,41 +2,32 @@ import mysql.connector
 
 
 class Database:
-    def connect(self):
-         #self.connection = mysql.connector.connect(
-         #    host="localhost",
-         #    user="root",
-         #    password="root123",
-         #   database="socialmedia"
-         #)
+    def __init__(self):
         self.connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            port = 3306,
+            port=3306,
             password="root123",
             database="socialmedia"
         )
+
+
         self.cursor = self.connection.cursor()
 
-    def disconnect(self):
+    def close(self):
+
         self.cursor.close()
         self.connection.close()
 
-    def transaction(self, sql_command, data=None):
-        self.connect()
-        if data:
-            self.cursor.execute(sql_command, data)
-        else:
-            self.cursor.execute(sql_command)
-        self.connection.commit()
-        self.disconnect()
+    def execute_sql(self, sql_command, data=None):
 
-    def report(self, sql_command, data=None):
-        self.connect()
-        if data:
-            self.cursor.execute(sql_command, data)
-        else:
-            self.cursor.execute(sql_command)
-        result = self.cursor.fetchall()
-        self.disconnect()
-        return result
+        self.cursor.execute(sql_command, data)
+
+        self.connection.commit()
+
+    def fetch_all_results(self, sql_command, data=None):
+        self.cursor.execute(sql_command, data)
+
+        results = self.cursor.fetchall()
+
+        return results
