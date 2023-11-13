e7c660727464d0c615736e0b313395cfcc7f9fd8
Student <sogolbasere@gmail.com>
2023-11-13 17:38:44 +0330

    updated

--- model/entity/like.py
+++ model/entity/like.py
@@ -1,5 +1,5 @@
 import re
-from sqlalchemy import Integer, String, Boolean, Date, ForeignKey
+from sqlalchemy import Integer, String, Boolean, Date, ForeignKey, Column
 from sqlalchemy.orm import relationship
 
 from model.entity.base import Base
@@ -9,8 +9,14 @@ from tools.validator import Validator
 class Like(Base):
     __tablename__ = "like_tbl"
 
-    def __init__(self, id, post, profile):
-        self.id = id
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("post.id"))
+    profile_id = Column(Integer, ForeignKey("profile.id"))
+
+    post = relationship("Post")
+    profile = relationship("Profile")
+
+    def __init__(self, post, profile):
         self.post = post
         self.profile = profile
 
@@ -20,33 +26,3 @@ class Like(Base):
     def to_tuple(self):
         return tuple(self.__dict__.values())
 
-    @property
-    def code(self):
-        return self._id
-
-    @code.setter
-    def id(self, id):
-        Validator.is_number(id, True, "Invalid Code")
-        self.id = id
-
-    @property
-    def post(self):
-        return self.post
-
-    @post.setter
-    def post(self, post):
-        if isinstance(post, str) and re.match("^[a-zA-Z\s]{2,30}$", post):
-            self.post = post
-        else:
-            raise ValueError("invalid post")
-
-    @property
-    def profile(self):
-        return self.profile
-
-    @profile.setter
-    def profile(self, profile):
-        if isinstance(profile, str) and re.match("^[a-zA-Z\s]{2,30}$", profile):
-            self._password = profile
-        else:
-            raise ValueError("invalid profile")
