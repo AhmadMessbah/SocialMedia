ebddeb8629ce151f19650f631b0003712306baa6
ariobarzandarvish18 <aryobarzandarvish@gmail.com>
2023-11-17 22:39:04 +0330

    updated

--- controller/profile_controller.py
+++ controller/profile_controller.py
@@ -1,52 +1,9 @@
-
-
 from model.da.profile_da import ProfileDa
 from model.entity.profile import Profile
 
 
+
 class PersonController:
-    @classmethod
-    def save(cls, name, family,username,password,email,image,status):
-        try:
-            profile = Profile(None,name,family,username,password,email,image,status)
-            da = ProfileDa()
-            return True, da.save(profile)
-        except Exception as e:
-            return False, str(e)
-
-    @classmethod
-    def edit(cls, code, name, family):
-        try:
-            person = Profile(code, name, family)
-            da = ProfileDa()
-            return True, da.edit(Profile)
-        except Exception as e:
-            return False, str(e)
-
-    @classmethod
-    def remove(cls, code):
-        try:
-            da = ProfileDa()
-            return True, da.remove(code)
-        except Exception as e:
-            return False, str(e)
-
-    @classmethod
-    def find_all(cls):
-        try:
-            da = ProfileDa()
-            return True, da.find_all()
-        except Exception as e:
-            return False, str(e)
-
-    @classmethod
-    def find_by_code(cls, code):
-        try:
-            da = ProfileDa()
-            return True, da.find_by_code(code)
-        except Exception as e:
-            return False, str(e)
-
     @classmethod
     def find_by_family(cls, family):
         try:
