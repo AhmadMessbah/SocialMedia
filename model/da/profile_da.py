781a15aeed4c1ebc2fc5890fdc41b3f0c06e2e7b
kimiyasd <lautnerfrg@gmail.com>
2023-11-22 15:28:10 +0330

    updated

--- model/da/profile_da.py
+++ model/da/profile_da.py
@@ -2,8 +2,19 @@ from model.da.database import DatabaseManager
 from model.entity.profile import Profile
 
 class ProfileDa(DatabaseManager):
-     def find_by_family(self, family):
-        self.make_engine()
-        result = self.session.query(Profile).filter(Profile.family.like(family+"%"))
-        self.session.close()
-        return result
+        def find_by_username_password(self, username, password):
+             self.make_engine()
+             result = self.session.query(Profile).filter(
+                 and_(Profile.username == username, Profile.password == password))
+             self.session.close()
+             return result
+     def find_by_username(self, username):
+         self.make_engine()
+         result = self.session.query(Profile).filter(Profile.username == username)
+         try:
+             if (result.username):
+                 self.session.close()
+                 return result
+         except:
+             return None
+
