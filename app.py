d2879391ea6dd400e273f86f1c77046fabfe36b3
ariobarzandarvish18 <aryobarzandarvish@gmail.com>
2023-11-29 10:18:26 +0330

    updated

--- app.py
+++ app.py
@@ -1,28 +1,51 @@
-from flask import *
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
 
-#from controller.profile_controller import PersonController
-
+from controllers.profile_controller import ProfileController
 app = Flask(__name__, template_folder="view", static_folder="view/assets")
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("template.html")
 
-#@app.route("/profile", methods = ["GET","POST", "DELETE"])
-#def profile():
-    #if request.method == "POST":
-     #   PersonController.save(
-      #      request.form.get("name"),
-       #     request.form.get("family"),
-        #    request.form.get("username"),
-         #   request.form.get("password"))
 
-    #if request.method == "DELETE":
-       # PersonController.remove(int(request.args.get("id")))
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # if find_by_username_password(username,password):
+        # print(username,password)
+        session["username"] = username
+        return render_template("profile.html", profile_list=ProfileController.find_all()[1])
+    return render_template("login.html")
 
-   # status,data = PersonController.find_all()
-    #if status:
-        #return render_template("profile.html", profile_list = data)
 
-app.run(host="", port="80", debug=True)
+@app.route("/profile", methods=["POST", "GET", "DELETE"])
+def profile():
+    if request.method == "POST":
+        name = request.form.get("name")
+        family = request.form.get("family")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        ProfileController.save(name, family, username, password)
+    elif request.method == "DELETE":
+        ProfileController.remove(request.args.get("id"))
+
+    # if not session.get("username"):
+    #     return render_template("login.html")
+    return render_template("profile.html", profile_list=ProfileController.find_all()[1])
+
+
+@app.route("/logout")
+def logout():
+    session["username"] = None
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ view/assets/js/profile-panel.js
@@ -0,0 +1,7 @@
+function showComments(postId) {
+    alert("Comments for postid " + postId);
+}
+
+function showLikes(postId) {
+    alert("Posts for postid " +postId);
+}
